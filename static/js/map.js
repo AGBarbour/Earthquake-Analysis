b0e441241842c830070345f3db638cf037b48eb6
AGBarbour <alex.g.barbour@gmail.com>
2018-10-25 18:30:57 -0500

    initialize repo and map

---
+++ static/js/map.js
@@ -0,0 +1,18 @@
+// Map variables
+var mapZoom = 4
+var mapCenter = [40, -95]
+
+// Create Map
+var myMap = L.map("map", {
+    center: mapCenter ,
+    zoom: mapZoom
+});
+  
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
+maxZoom: 18,
+id: "mapbox.streets",
+accessToken: API_KEY
+}).addTo(myMap);
+
+
